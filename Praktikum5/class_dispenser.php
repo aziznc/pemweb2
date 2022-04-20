2093835f8eae87f79f837d2eb6a324c911e1515f
aziznc <79524111+aziznc@users.noreply.github.com>
2022-04-20 14:13:43 +0700

    Add files via upload

---
+++ Praktikum5/class_dispenser.php
@@ -0,0 +1,16 @@
+<?php
+class Dispenser{
+    protected $volume;
+    protected $hargaSegelas;
+    private $volumeGelas;
+    public $namaMinuman;
+
+    public isi($vol){
+        $this->volume = $vol;
+    }
+}
+
+
+
+
+?>
